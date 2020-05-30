a85b9416cb134c3acbf4336c7ad0752f35a65704
rkz1337 <booster2228@yandex.ru>
2020-05-30 16:26:29 +0300

    Add vendor changes + new routes

--- routes/web.php
+++ routes/web.php
@@ -16,21 +16,52 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'HomeController@index')->name('home');
 
 // Авторизация
-Route::get('/signup', 'AuthController@getSignup')->middleware('guest')->name('auth.signup');
-Route::post('/signup', 'AuthController@postSignup')->middleware('guest');
-
-Route::get('/signin', 'AuthController@getSignin')->middleware('guest')->name('auth.signin');
-Route::post('/signin', 'AuthController@postSignin')->middleware('guest');
-
-Route::get('/signout', 'AuthController@getSignout')->name('auth.signout');
+Route::get('/signup', 'AuthController@getSignup')
+        ->middleware('guest')
+        ->name('auth.signup');
+Route::post('/signup', 'AuthController@postSignup')
+        ->middleware('guest');
+Route::get('/signin', 'AuthController@getSignin')
+        ->middleware('guest')
+        ->name('auth.signin');
+Route::post('/signin', 'AuthController@postSignin')
+        ->middleware('guest');
+Route::get('/signout', 'AuthController@getSignout')
+        ->name('auth.signout');
 
 //Поиск
-Route::get('/search', 'SearchController@getResults')->name('search.results');
+Route::get('/search', 'SearchController@getResults')
+        ->name('search.results');
 
 //Профили
-Route::get('/personnel/{id}/edit', 'ProfileController@editProfile')->middleware('auth')->name('profile.edit');
+Route::get('/personnel/all', 'ProfileController@allProfiles')
+        ->middleware('auth')
+        ->name('profile.all');
 
-Route::post('/personnel/{id}/edit', 'ProfileController@editProfileSubmit')->middleware('auth')->name('profile.edit.submit');
+Route::get('/personnel/all/create', 'ProfileController@createProfiles')
+        ->middleware('auth')
+        ->name('profile.create');
+Route::post('/personnel/all/create', 'ProfileController@createProfilesSubmit')
+        ->middleware('auth')
+        ->name('profile.create.submit');
+
+Route::get('/personnel/{id}/edit', 'ProfileController@editProfile')
+        ->middleware('auth')
+        ->name('profile.edit');
+        
+Route::post('/personnel/{id}/edit', 'ProfileController@editProfileSubmit')
+        ->middleware('auth')
+        ->name('profile.edit.submit');
+
+Route::get('/user/{id}/edit', 'ProfileController@editUser')
+        ->middleware('auth')
+        ->name('user.edit');
+        
+Route::post('/user/{id}/edit', 'ProfileController@editUserSubmit')
+        ->middleware('auth')
+        ->name('user.edit.submit');
+
+
 
 
 
